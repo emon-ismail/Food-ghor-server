1509ddb24d8101f4addf86717248492c798cb735
Tonmoy-Roy <emonismail69@gmail.com>
2022-11-09 23:23:21 +0600

    some functionality updated

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
 
-// const cors = require('cors');
-// require ("dotenv").config
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// app.use(cors());
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-//   })
-//   app.listen(port, () => {
-//     console.log(`Example app listening on port ${process.env.PORT}`)
-//   })
-// console.log(process.env.DB_USER)
-// user:foodghor
-// pass:VyAKqH1Z2M9Yhktp
-
-
-
-
-// const uri = "mongodb+srv://foodghor:VyAKqH1Z2M9Yhktp@cluster0.ww1mwol.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
-
-// ...............jj
 
 const express = require('express');
 const cors = require('cors');
@@ -40,15 +12,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
-const uri = "mongodb+srv://foodghor:VyAKqH1Z2M9Yhktp@cluster0.ww1mwol.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         const serviceCollection = client.db('food').collection('services');
-        const orderCollection = client.db('geniusCar').collection('orders');
+        const reviewCollection = client.db('food').collection('reviews');
 
         app.get('/services', async (req, res) => {
             const query = {}
@@ -66,7 +38,7 @@ async function run() {
 
 
         // orders api
-        app.get('/orders', async (req, res) => {
+        app.get('/reviews', async (req, res) => {
             let query = {};
 
             if (req.query.email) {
@@ -75,18 +47,18 @@ async function run() {
                 }
             }
 
-            const cursor = orderCollection.find(query);
-            const orders = await cursor.toArray();
-            res.send(orders);
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
         });
 
-        app.post('/orders', async (req, res) => {
+        app.post('/reviews', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
         });
 
-        app.patch('/orders/:id', async (req, res) => {
+        app.patch('/reviews/:id', async (req, res) => {
             const id = req.params.id;
             const status = req.body.status
             const query = { _id: ObjectId(id) }
@@ -95,14 +67,14 @@ async function run() {
                     status: status
                 }
             }
-            const result = await orderCollection.updateOne(query, updatedDoc);
+            const result = await reviewCollection.updateOne(query, updatedDoc);
             res.send(result);
         })
 
-        app.delete('/orders/:id', async (req, res) => {
+        app.delete('/reviews/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const result = await orderCollection.deleteOne(query);
+            const result = await reviewCollection.deleteOne(query);
             res.send(result);
         })
 
